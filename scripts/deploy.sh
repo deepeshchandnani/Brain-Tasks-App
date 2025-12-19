9029a5eef73ac5967a702eceb25e5027b75a5aa9
deepeshchandnani <85671558+deepeshchandnani@users.noreply.github.com>
2025-12-19 17:08:37 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,10 @@
 set -e
 
 echo "Updating kubeconfig..."
-aws eks update-kubeconfig \
-  --region ap-south-1 \
-  --name brain-eks
+aws eks update-kubeconfig --region ap-south-1 --name brain-eks
 
 echo "Deploying to EKS..."
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
 
 echo "Deployment completed"
